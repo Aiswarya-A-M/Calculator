2a2dcb86a2a34cf7dcfe06470926f0d279c2ca63
Aiswarya-A-M <148937898+Aiswarya-A-M@users.noreply.github.com>
2023-10-26 15:55:15 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,38 +1,15 @@
-let numArray=[];
 function screenDisplay(value) {
-    const display = document.getElementById("screen");
+    const display = document.getElementById("screen");        //function to display the button value.
     display.value += value;
 }
 function bclear(){
-    const display = document.getElementById("screen");
+    const display = document.getElementById("screen");    //function t clear the display   
     display.value="";
-    numArray=[];
 }
 
 function calculateResult(){
-    const display = document.getElementById("screen");
+    const display = document.getElementById("screen");   // function to calculate result
     let expression=display.value;
     let result=eval(expression);
     document.getElementById("screen").value=result;
-    //operator=numArray[0];
-    //console.log("array of operator",operator,numArray);
-    
-    /*let result=0;
-    console.log("ii...",1*2+10)
-    for (let i=0;i<arr.length;i++){
-        switch (operator){
-            case '+':
-                result+=arr[i];
-            case '-':
-                result-=arr[i];
-            case '*':
-                result*=arr[i];
-            case '/':
-                result/=arr[i];
-            default:
-                result=0;
-        }
-    }*/
-    
-
 }
