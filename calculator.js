d2110946b3203b43ad73b126ca88f5894a63fc4b
Aiswarya-A-M <148937898+Aiswarya-A-M@users.noreply.github.com>
2023-10-26 15:56:28 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -3,12 +3,12 @@ function screenDisplay(value) {
     display.value += value;
 }
 function bclear(){
-    const display = document.getElementById("screen");    //function t clear the display   
+    const display = document.getElementById("screen");    //function t clear the screen.  
     display.value="";
 }
 
 function calculateResult(){
-    const display = document.getElementById("screen");   // function to calculate result
+    const display = document.getElementById("screen");   // function to calculate result.
     let expression=display.value;
     let result=eval(expression);
     document.getElementById("screen").value=result;
