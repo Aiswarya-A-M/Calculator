560add13d0f44ff60ac7f9edb10131d6217a4c5a
Aiswarya-A-M <148937898+Aiswarya-A-M@users.noreply.github.com>
2023-10-27 13:17:42 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,15 +1,18 @@
 let displayValue = '';
 let resultShown = false;
+let decimal=false;
 function screenDisplay(value) {
     const display = document.getElementById("screen");
+    const length=displayValue.length - 1;
+    const lastChar = displayValue.charAt(displayValue.length - 1);
     if (resultShown){
         if (value === '+' || value === '-' || value === '*' || value === '/') {
-            displayValue = display.value + value;
+            display.value=displayValue;                                                
             resultShown = false;
         } else {
             resultShown = false;
             clearDisplay();
-        }
+        }  
     }
     if (displayValue==="Cannot divide by zero") {
         clearDisplay();
@@ -17,7 +20,27 @@ function screenDisplay(value) {
     if (displayValue === '' && (value === '+' || value === '-' || value === '*' || value === '/')) {
         return; // Prevent operators when the screen is empty
     }
-    const lastChar = displayValue.charAt(displayValue.length - 1);
+    if (value==='.' && displayValue.toString().includes(".")){
+        let count=0;
+        for (let i=length;i>=0;i--){
+            if (displayValue.charAt(i)==='.'){
+                return;
+            }
+            else if(displayValue.charAt(i)==='+'||displayValue.charAt(i)==='-'||displayValue.charAt(i)==='*'||displayValue.charAt(i)==='/'){
+                    count=0;
+                    displayValue += value;
+                    display.value = displayValue;
+                }
+            else{
+                count+=1;
+            }
+                    
+        }
+        if(count!==0){
+            return;
+        }
+            
+    }
     if (value === '=' && displayValue) {
         const result = eval(displayValue);
         const fixedResult = parseFloat(result.toFixed(10));
@@ -36,22 +59,21 @@ function screenDisplay(value) {
         resultShown = false;
     }
     else {
-        if (value === '+' || value === '-' || value === '*' || value === '/') {
-      // Check if the last character in the display is an operator
-            if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
-                return; // Prevent consecutive operators
+        if (value === '+' || value === '-' || value === '*' || value === '/' ) {
+      // Check if the last character in the display is an operator or decimal
+            if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/' ) {
+                    return;
             }
-      
         }
-        displayValue += value;
-        display.value = displayValue;
-    
     }
+    displayValue += value;
+    display.value = displayValue;
 }
 
 function clearDisplay() {        //to clear the display.
     displayValue = '';
     document.getElementById("screen").value = '';
+    
 }
 
 function calculateResult() {              //to calculate result.
@@ -65,7 +87,10 @@ function calculateResult() {              //to calculate result.
         display.value = fixedResult;
         displayValue = fixedResult.toString();
         resultShown=true;
-
     } 
 }
 
+function deleteDisplay() {       //to delete last entered
+    displayValue=displayValue.toString().slice(0,-1)
+    document.getElementById("screen").value =displayValue;
+}
